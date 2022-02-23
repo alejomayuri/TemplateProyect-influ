581569879fa4490067e944f8779209117961c49a
Alejandro} <alejomayuri@gmail.com>
2022-02-23 00:36:47 -0600

    desarrollo

---
+++ js/code.js
@@ -0,0 +1,40 @@
+$(document).ready(function () {
+    $('.video-button').click(function () {
+        $('.modal-video').css('display', 'block')
+    })
+    $('.modal-video-close-btn').click(function () {
+        $('.modal-video').css('display', 'none')
+    })
+    $('.modal-video').click(function () {
+        $('.modal-video').css('display', 'none')
+    })
+})
+
+// global variable for the player
+var player;
+
+// this function gets called when API is ready to use
+function onYouTubePlayerAPIReady() {
+    // create the global player from the specific iframe (#video)
+    player = new YT.Player('video', {
+        events: {
+            // call this function when player is ready to use
+            'onReady': onPlayerReady
+        }
+    });
+}
+
+
+function onPlayerReady(event) {
+
+    // bind events
+    var playButton = document.getElementById("play-button");
+    playButton.addEventListener("click", function () {
+        player.playVideo();
+    });
+
+    var stopButton = document.getElementById("stop-button");
+    stopButton.addEventListener("click", function () {
+        player.stopVideo();
+    });
+}
